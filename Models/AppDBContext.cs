9e129a29fdd8918a6939df50997b0dfd71fec715
k-szymaniak <80097559+k-szymaniak@users.noreply.github.com>
2022-01-07 10:02:08 +0100

    Add files via upload

---
+++ Models/AppDBContext.cs
@@ -0,0 +1,28 @@
+﻿using CarRent.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_3._1.Models
+{
+    public class AppDBContext : IdentityDbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+        public DbSet<CarModel> cars { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+        
+    }
+}
---
+++ Models/CarModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace CarRent.Models
+{
+    [Table("Cars")]
+    public class CarModel
+    {
+        [Key]
+        public int CarId { get; set; }
+        [Required (ErrorMessage = "Required field")]
+        public string CarMark { get; set; }
+        [Required(ErrorMessage = "Required field")]
+        public string Model { get; set; }
+        [Required(ErrorMessage = "Required field")]
+
+        public double Power { get; set; }
+        [Required(ErrorMessage = "Required field")]
+
+        public string FuelType { get; set; }
+        [Required(ErrorMessage = "Required field")]
+        public double Price { get; set; }
+        [Required(ErrorMessage = "Required field")]
+        public double Category { get; set; }
+        [Required(ErrorMessage = "Required field")]
+        public double Mileage { get; set; }
+        [Required(ErrorMessage = "Required field")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Required field")]
+        public bool Availability { get; set; }
+
+    }
+}
