e833307e2840e2850418463d4ad0fe96e2f84590
k-szymaniak <k-szymaniak@tlen.pl>
2022-01-26 11:42:25 +0100

    26.01.2022

--- Controllers/CarController.cs
+++ Controllers/CarController.cs
@@ -18,127 +18,133 @@ namespace CarRent.Controllers
             _carRepository = carRepository;
         }
 
+        public CarController()
+        {
+        }
+
+
+
 
         //Cars
 
         // GET: CarController
         public ActionResult Index()
-        {
-            
-            return View(_carRepository.GetAll());
-        }
-        public ActionResult Cars()
-        {
+            {
 
-            return View(_carRepository.GetAll());
-        }
-        public ActionResult Motorcycles()
-        {
+                return View(_carRepository.GetAll());
+            }
+            public ActionResult Cars()
+            {
 
-            return View(_carRepository.GetAll());
-        }
-        public ActionResult Others()
-        {
+                return View(_carRepository.GetAll());
+            }
+            public ActionResult Motorcycles()
+            {
 
-            return View(_carRepository.GetAll());
-        }
-        public ActionResult Contact()
-        {
+                return View(_carRepository.GetAll());
+            }
+            public ActionResult Others()
+            {
 
-            return View(nameof(Contact));
-        }
+                return View(_carRepository.GetAll());
+            }
+            public ActionResult Contact()
+            {
+
+                return View(nameof(Contact));
+            }
 
 
 
-        public ActionResult Admin()
-        {
-            return View(_carRepository.GetAll());
-        }
+            public ActionResult Admin()
+            {
+                return View(_carRepository.GetAll());
+            }
 
-        // GET: CarController/Details/5
-        [Authorize]
-        public ActionResult Details(int id)
-        {
-            return View(_carRepository.Get(id));
-        }
+            // GET: CarController/Details/5
+            [Authorize]
+            public ActionResult Details(int id)
+            {
+                return View(_carRepository.Get(id));
+            }
 
-        // GET: CarController/Create
-        [Authorize(Roles = "Admin")]
-        public ActionResult Create()
-        {
-            return View(new CarModel());
-        }
+            // GET: CarController/Create
+            [Authorize(Roles = "Admin")]
+            public ActionResult Create()
+            {
+                return View(new CarModel());
+            }
 
-        // POST: CarController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(CarModel carModel)
-        {
-            _carRepository.Add(carModel);
+            // POST: CarController/Create
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Create(CarModel carModel)
+            {
+                _carRepository.Add(carModel);
 
-            return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index));
 
-        }
+            }
 
-        // GET: CarController/Edit/5
-        [Authorize(Roles = "Admin")]
-        public ActionResult Edit(int id)
-        {
-            return View(_carRepository.Get(id));
-        }
+            // GET: CarController/Edit/5
+            [Authorize(Roles = "Admin")]
+            public ActionResult Edit(int id)
+            {
+                return View(_carRepository.Get(id));
+            }
 
-        // POST: CarController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, CarModel carModel)
-        {
-            _carRepository.Update(id, carModel);
+            // POST: CarController/Edit/5
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Edit(int id, CarModel carModel)
+            {
+                _carRepository.Update(id, carModel);
 
-            return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index));
 
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult ChangeAvailability(int id, CarModel carModel)
-        {
-            _carRepository.ChangeAvailability(id, carModel);
+            }
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult ChangeAvailability(int id, CarModel carModel)
+            {
+                _carRepository.ChangeAvailability(id, carModel);
 
-           
+
 
                 return RedirectToAction(nameof(Index));
 
 
-        }
+            }
 
-        public ActionResult ChangeAvailability(int id)
-        {
-            return View(_carRepository.Get(id));
-        }
-
-
-
-        // GET: CarController/Delete/5
-        [Authorize(Roles = "Admin")]
-        public ActionResult Delete(int id)
-        {
-            return View(_carRepository.Get(id));
-        }
-
-        // POST: CarController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, CarModel carModel)
-        {
-            _carRepository.Delete(id);
-            return RedirectToAction(nameof(Index));
-
-
-        }
+            public ActionResult ChangeAvailability(int id)
+            {
+                return View(_carRepository.Get(id));
+            }
 
 
 
+            // GET: CarController/Delete/5
+            [Authorize(Roles = "Admin")]
+            public ActionResult Delete(int id)
+            {
+                return View(_carRepository.Get(id));
+            }
+
+            // POST: CarController/Delete/5
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Delete(int id, CarModel carModel)
+            {
+                _carRepository.Delete(id);
+                return RedirectToAction(nameof(Index));
 
 
+            }
+
+
+
+
+        
     }
 }
 
